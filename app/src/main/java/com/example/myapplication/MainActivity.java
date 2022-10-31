c82fd38843656ff7f7f1c051bd8308ddfe4805ef
QTrungLv <trunglvnqt2901@gmail.com>
2022-11-01 02:24:21 +0700

    Bai1

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView number = null;
+    Button toast = null;
+    Button count = null;
+
+    int countNumber = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        number = findViewById(R.id.numberCount);
+        number.setText(""+countNumber);
+        toast = findViewById(R.id.toast);
+        count = findViewById(R.id.count);
+
+        count.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(number != null){
+                    countNumber++;
+                    number.setText("" + countNumber);
+
+                }
+
+            }
+        });
+
+
     }
 }
